235c4fec65a6ef983a6a8029d66660afac3d262f
Nick Gray <nickgray87@gmail.com>
2021-01-20 00:39:11 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -67,7 +69,7 @@ def adding_matz
 # return the entire updated hash
 
 	programmer_hash = 
- 		{
+ 		{ 
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +83,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+ 		  }
+ 	programmer_hash
 end
 
 def changing_alan
@@ -103,6 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 
 end
 
@@ -125,5 +133,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 
 end

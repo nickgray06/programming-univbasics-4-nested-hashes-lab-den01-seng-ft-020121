82da533ffa91ac9d8858e5878b69e56e48fbc889
Nick Gray <nickgray87@gmail.com>
2021-01-20 00:37:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 
 end
 
@@ -131,5 +133,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages] = 
 
 end
